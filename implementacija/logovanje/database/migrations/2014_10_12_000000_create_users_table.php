c679f243ac87219876b965d9002f71b39a21605d
mdivjak <marko.d@europe.com>
2020-05-08 06:35:14 -0700

    Napravljeno logovanje i registracija, pravljenje turnira sa validacijom podataka

---
+++ implementacija/logovanje/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('bAdmin')->default('0');
+            $table->integer('cntPokemons')->default('0');
+            $table->integer('cntBalls')->default('0');
+            $table->integer('cntFruits')->default('0');
+            $table->integer('cntCash')->default('0');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
