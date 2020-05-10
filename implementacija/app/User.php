04a500c5e8674e902df7fc78e68559d179487fcd
mdivjak <marko.d@europe.com>
2020-05-10 14:57:16 -0700

    Vracene nagrade u kvizu za ulogovane korisnike (slucajno bile obrisane)

--- implementacija/app/User.php
+++ implementacija/app/User.php
@@ -11,8 +11,8 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    public function addPokeCash ($amount) {
-        $this->cntCash += $amount;
+    public function addPokeCash () {
+        $this->cntCash += env('QUIZ_PRIZE');
         $this->save();
     }
     
